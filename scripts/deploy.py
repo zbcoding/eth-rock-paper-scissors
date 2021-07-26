eec7561165493650e2129000d372350029ae4352
zbcoding <uszagb@gmail.com>
2021-07-26 01:15:39 -0500

    Fixed ERC20Basic Error and Brownie Erros, contract deploys!

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -1,9 +1,9 @@
-from brownie import Yield, config, accounts
+from brownie import ERC20Basic, config, accounts
 
 def deployContract():
 	account = accounts.add(config["wallets"]["from_key"]) or accounts[0]
 	#the Yield contract constructor only needs a total supply parameter
-	return Yield.deploy( 1000e8, {'from': account})
+	return ERC20Basic.deploy( 1000e8, {'from': account})
 
 def main():
 	deployContract()
